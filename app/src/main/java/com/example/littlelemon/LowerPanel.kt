b635fab876ba27f379ef980cf4ab48091b222f8f
AouinaneMoussa <moussa.aouinane@gmail.com>
2023-12-20 19:40:58 +0100

    CreateMenuDishComposable

--- app/src/main/java/com/example/littlelemon/LowerPanel.kt
+++ app/src/main/java/com/example/littlelemon/LowerPanel.kt
@@ -1,17 +1,25 @@
 package com.example.littlelemon
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import java.nio.file.WatchEvent
 
 @Composable
 fun LowerPanel(navController: NavHostController, dishes: List<Dish> = listOf()) {
@@ -39,7 +47,7 @@ fun WeeklySpecialCard() {
         )
     }
 }
-
+//Create the MenuDish Composable
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun MenuDish(navController: NavHostController? = null, dish: Dish) {
@@ -48,6 +56,34 @@ fun MenuDish(navController: NavHostController? = null, dish: Dish) {
         navController?.navigate(DishDetails.route + "/${dish.id}")
     }) {
         //TODO: Insert code here
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ){
+            Column {
+                Text(
+                    text = dish.name,
+                    style = MaterialTheme.typography.h2
+                )
+                Text(
+                    text = dish.description,
+                    style = MaterialTheme.typography.body1,
+                    modifier = Modifier
+                        .fillMaxWidth(0.75f)
+                        .padding(top = 5.dp, bottom = 5.dp)
+                )
+                Text(
+                    text = "$dish.price",
+                    style = MaterialTheme.typography.body2
+                )
+            }
+            Image(
+                painter = painterResource(id = dish.imageResource),
+                contentDescription = "Dish Image",
+                modifier = Modifier.clip(RoundedCornerShape(10.dp))
+            )
+        }
     }
     Divider(
         modifier = Modifier.padding(start = 8.dp, end = 8.dp),
